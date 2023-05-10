e71cd7cb479306a741fa5aadbbcde8aa109efc77
GGFlutterdev <luigi.allocca12345@live.it>
2023-05-10 19:54:03 +0200

    Updated DataSourceResolverTest

--- src/test/java/org/apache/commons/mail/resolver/DataSourceFileResolverTest.java
+++ src/test/java/org/apache/commons/mail/resolver/DataSourceFileResolverTest.java
@@ -24,7 +24,9 @@ import java.io.IOException;
 
 import org.apache.commons.mail.DataSourceResolver;
 import org.junit.Test;
-import org.junit.jupiter.api.function.Executable;
+import javax.activation.DataSource;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import static org.evosuite.runtime.EvoAssertions.*;
 
 /**
  * JUnit test case for DateSourceResolver.
@@ -33,6 +35,120 @@ import org.junit.jupiter.api.function.Executable;
  */
 public class DataSourceFileResolverTest extends AbstractDataSourceResolverTest
 {
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        DataSourceFileResolver dataSourceFileResolver0 = new DataSourceFileResolver((File) null);
+        File file0 = dataSourceFileResolver0.getBaseDir();
+        assertNull(file0);
+    }
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        DataSourceFileResolver dataSourceFileResolver0 = new DataSourceFileResolver();
+        File file0 = dataSourceFileResolver0.getBaseDir();
+        assertTrue(file0.exists());
+    }
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        DataSourceFileResolver dataSourceFileResolver0 = new DataSourceFileResolver();
+        // Undeclared exception!
+        try {
+            dataSourceFileResolver0.resolve((String) null, false);
+            fail("Expecting exception: NullPointerException");
+
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.mail.resolver.DataSourceBaseResolver", e);
+        }
+    }
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        DataSourceFileResolver dataSourceFileResolver0 = new DataSourceFileResolver();
+        // Undeclared exception!
+        try {
+            dataSourceFileResolver0.resolve((String) null);
+            fail("Expecting exception: NullPointerException");
+
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.mail.resolver.DataSourceBaseResolver", e);
+        }
+    }
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        DataSourceFileResolver dataSourceFileResolver0 = new DataSourceFileResolver((File) null);
+        try {
+            dataSourceFileResolver0.resolve("", false);
+            fail("Expecting exception: IOException");
+
+        } catch (IOException e) {
+            //
+            // Cant resolve the following file resource
+            // :/Users/luigiallocca/development/commons-email/.
+            //
+            verifyException("org.apache.commons.mail.resolver.DataSourceFileResolver", e);
+        }
+    }
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        DataSourceFileResolver dataSourceFileResolver0 = new DataSourceFileResolver();
+        DataSource dataSource0 = dataSourceFileResolver0.resolve("/J@3Bl-+A)qj2", true);
+        assertNull(dataSource0);
+    }
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        DataSourceFileResolver dataSourceFileResolver0 = new DataSourceFileResolver((File) null, true);
+        DataSource dataSource0 = dataSourceFileResolver0.resolve("cid:", true);
+        assertNull(dataSource0);
+    }
+
+    @Test(timeout = 4000)
+    public void test08() throws Throwable {
+        MockFile mockFile0 = new MockFile("", "");
+        DataSourceFileResolver dataSourceFileResolver0 = new DataSourceFileResolver(mockFile0);
+        File file0 = dataSourceFileResolver0.getBaseDir();
+        assertFalse(file0.isFile());
+    }
+
+    @Test(timeout = 4000)
+    public void test09() throws Throwable {
+        MockFile mockFile0 = new MockFile("");
+        DataSourceFileResolver dataSourceFileResolver0 = new DataSourceFileResolver(mockFile0);
+        DataSource dataSource0 = dataSourceFileResolver0.resolve("");
+        assertNotNull(dataSource0);
+    }
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        DataSourceFileResolver dataSourceFileResolver0 = new DataSourceFileResolver();
+        try {
+            dataSourceFileResolver0.resolve("oEN/kK7mNV/,v1e&/v");
+            fail("Expecting exception: IOException");
+
+        } catch (IOException e) {
+            //
+            // Cant resolve the following file resource
+            // :/Users/luigiallocca/development/commons-email/./oEN/kK7mNV/,v1e&/v
+            //
+            verifyException("org.apache.commons.mail.resolver.DataSourceFileResolver", e);
+        }
+    }
+
+    @Test(timeout = 4000)
+    public void test11() throws Throwable {
+        DataSourceFileResolver dataSourceFileResolver0 = new DataSourceFileResolver((File) null, true);
+        DataSource dataSource0 = dataSourceFileResolver0.resolve("cid:");
+        assertNull(dataSource0);
+    }
 
     @Test
     public void testResolvingFileLenient() throws Exception
